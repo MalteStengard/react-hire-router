36fdf385ea41aa6e7ba1f62faf9d3360974904b4
Malte <malte.stengard@gmail.com>
2024-10-02 08:54:30 +0200

    core and extension done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,69 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import { useNavigate, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import PersonProfile from './pages/PersonProfile';
 
 export default function App() {
   const [hiredPeople, setHiredPeople] = useState([])
+  const [people, setPeople] = useState([])
+
+  const fetchPeople = async () => {
+    const response = await fetch('https://randomuser.me/api/?results=50');
+    const jsonData = await response.json();
+    setPeople(jsonData.results);
+    console.log(jsonData.results)
+  }
+
+  useEffect(() => {
+      fetchPeople();
+  }, []);
+
+  const navigate = useNavigate();
+
+  const handleNavigation = (path) => {
+    navigate(path);
+  };
 
   return (
-    <>
+    <div>
       <header>
-        <h1>Hire Your Team</h1>
+        <h1>People Market</h1>
         <nav>
           <ul>
-            <li>Dashboard</li>
+            <li>
+              <button onClick={() => handleNavigation('/dashboard')}>View Dashboard</button>
+            </li>
+            {/* <li>
+              <button onClick={() => handleNavigation('/profiles')}>View Profiles</button>
+            </li> */}
+            <li>
+              <button onClick={() => handleNavigation('/')}>Home</button>
+            </li>
           </ul>
         </nav>
       </header>
-    </>
-  )
+      <main>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard hiredPeople={hiredPeople} people={people} />} />
+          <Route path="/View/:id" element={<PersonProfile people = {people} hiredPeople = {hiredPeople} setHiredPeople={setHiredPeople}/>} />
+          <Route path="/" />
+        </Routes>
+      </main>
+    </div>
+  );
 }
+
+// function fetchPeople() {
+//   const fetchPeople = async () => {
+//     const response = await fetch('https://randomuser.me/api/?results=50');
+//     const jsonData = await response.json();
+//     return jsonData.results;
+//   }
+
+//   // useEffect(() => {
+//   //   if (!people || people.length === 0) {
+//   //     fetchPeople();
+//   //   }
+//   // }, []);
+// }
--- src/pages/Dashboard/components/PeopleListItem.jsx
+++ src/pages/Dashboard/components/PeopleListItem.jsx
@@ -1,14 +1,18 @@
-function PeopleListItem(props) {
-  const { person } = props
+import HireForm from "../../PersonProfile/components/HireForm"
+import { Link } from 'react-router-dom';
 
+function PeopleListItem({person, index}) {
   return (
     <li>
       <h3>
+      <Link to={`/view/${index}`}>
         {person.name.first} {person.name.last}
+        </Link>
       </h3>
       {person.wage && <p>Wage: £{person.wage}</p>}
     </li>
   )
 }
+
 
 export default PeopleListItem
